e57812d981bad7db1fb938d2b743afab7ef44f2b
qqgameover <kasper35@live.no>
2021-10-08 15:21:46 +0200

    Initial commit

---
+++ ObligUno/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObligUno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
